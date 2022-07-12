d87259882165daf57c4f3c33e4fd05095f146092
Luketedwards <Luketedwards96@hotmail.com>
2022-07-12 12:10:59 +0000

    added categories to product model

--- products/models.py
+++ products/models.py
@@ -23,11 +23,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True,)
     vendor = models.CharField(max_length=254, null=True)
-    PDF = models.FileField(null=True)
-    Guitar_Pro = models.FileField(null=True)
+    PDF = models.FileField(null=True,)
+    Guitar_Pro = models.FileField(null=True,)
     video = models.URLField(max_length=1024, null=True, blank=True)
+    
 
 
     def __str__(self):
