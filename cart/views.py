4ffa5f8eff4d90ad045482183e90e6e752f97fdb
Luketedwards <Luketedwards96@hotmail.com>
2022-07-18 20:09:19 +0000

    Added to checkout and shopping bag

--- cart/views.py
+++ cart/views.py
@@ -14,7 +14,7 @@ def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the cart """
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
+    quantity = 1
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
@@ -59,7 +59,8 @@ def remove_from_cart(request, item_id):
         messages.success(request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
-        return HttpResponse(status=200)
+        
+        return redirect(reverse('view_cart'))
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
