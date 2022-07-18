9df64a482853a7da0dcef33a253102b901eb817f
Luketedwards <Luketedwards96@hotmail.com>
2022-07-18 19:38:36 +0000

    added cart contexts, template tags, views and template

--- score_emporium/urls.py
+++ score_emporium/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('profile/', include('user_profile.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
