56fa883d0f37776e3d275826f6fb9b3ed12b7173
Luketedwards <Luketedwards96@hotmail.com>
2022-08-08 12:02:04 +0000

    set event listener for scrolling ove reviews panel

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,24 @@ $(document).ready(function(){
   $('#myModal').modal('show');  
 
 
+  var observer = new IntersectionObserver(function(entries) {
+    // isIntersecting is true when element and viewport are overlapping
+    // isIntersecting is false when element and viewport don't overlap
+    if(entries[0].isIntersecting === true)
+      $('#5bar').removeClass('empty')
+      $('#5bar').addClass('full5')
+      $('#4bar').removeClass('empty')
+      $('#4bar').addClass('full4')
+      $('#3bar').removeClass('empty')
+      $('#3bar').addClass('full3')
+      $('#2bar').removeClass('empty')
+      $('#2bar').addClass('full2')
+      $('#1bar').removeClass('empty')
+      $('#1bar').addClass('full1')
+  }, { threshold: [0] });
+  
+  observer.observe(document.querySelector(".graphs"));
+
   /* code for converting star rating to input value of review*/
 
   document.getElementById('rating-5-click').addEventListener('click', rating5);
@@ -54,4 +72,5 @@ $('.carousel').carousel({
   $(".product").hover(function(){
     $(this).toggleClass("is-active");
   });
+  
   
