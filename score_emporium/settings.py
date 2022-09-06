a2a0dae20b8265bc77da25cc66d9907a60a41b1e
Luketedwards <Luketedwards96@hotmail.com>
2022-09-06 10:27:38 +0000

    removed secret key and set debug to false

--- score_emporium/settings.py
+++ score_emporium/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bl#-fxm0tcra0%y#2r_^7n*a3&eb*9$!ah+mbis=5zrx44u=wm'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['score-emporium.herokuapp.com', 'localhost']
 
