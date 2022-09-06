090d42888b807bcc1da156b36a5e66359de18222
Luketedwards <Luketedwards96@hotmail.com>
2022-09-06 10:21:43 +0000

    fixed reviews error on index page

--- home/views.py
+++ home/views.py
@@ -8,16 +8,11 @@ def index(request):
     """ A view to return the index page """
     reviews = Review.objects.all().order_by('-ratings')
     reviews = reviews[:3]
-    review1 = reviews[random.randint(0, len(reviews) - 1)]
-    review2 = reviews[random.randint(0, len(reviews) - 1)]
-    number1 = random.randint(0, len(reviews) - 1)
-    number2 = random.randint(0, len(reviews) - 1)
+    
+
     
     context = {
-        'review1': review1,
-        'review2': review2,
-        'number1': number1,
-        'number2': number2,
+        
         'reviews': reviews
     }
 
