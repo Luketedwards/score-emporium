9a22d2b399b170bd606d583792330d762e88cd0c
Luketedwards <Luketedwards96@hotmail.com>
2022-09-07 12:55:31 +0000

    changed edit function

--- products/views.py
+++ products/views.py
@@ -357,7 +357,57 @@ def edit_product(request, product_id):
         if request.method == 'POST':
             form = ProductForm(request.POST, request.FILES, instance=product)
             if form.is_valid():
-                form.save()
+                # delete existing files and replace with new ones
+                delete_from_s3(product_id)
+                obj = form.save(commit=False)
+                obj.vendor = request.user.username
+                name = f"{obj.name}"
+                name2 = obj.PDF.name
+                new_name = name.replace(" ","-")
+                new_name2 = name2.replace(" ","-")
+                if not obj.image:
+                    font = ImageFont.truetype('fonts/PlayfairDisplay-Bold.ttf', 400)
+                    font2 = ImageFont.truetype('fonts/PlayfairDisplay-Bold.ttf', 100)
+                    text = 'Sample'
+                    text2 = f'© {obj.vendor}'
+
+                    fs = FileSystemStorage(location=UPLOAD_ROOT, base_url='/uploads')
+                    pdfFile = fs.save(f"{new_name}-{obj.vendor}.pdf", obj.PDF)
+                    pdfPath = fs.path(pdfFile)
+
+                    pdf = pdfium.PdfDocument(pdfPath)
+                    page = pdf.get_page(0)
+                    pil_image = page.render_topil()
+                    pil_image.save(f"pil-{obj.name}-{obj.vendor}.jpg")
+                    page.close()
+
+                    image = Image.open(f'pil-{obj.name}-{obj.vendor}.jpg')
+                    output = BytesIO()
+                    cropped_image = image.crop((5,1673,2459,3313))
+                    blurred_image = cropped_image.filter(ImageFilter.GaussianBlur(radius=10))
+                    image.paste(blurred_image,(5,1673,2459,3313))
+                    editImage = ImageDraw.Draw(image)
+                    editImage.text((550,2100), text,(84, 83, 82), font=font)
+                    editImage2 = ImageDraw.Draw(image)
+                    editImage2.text((850,2600), text2,(84, 83, 82), font=font2)
+                    image.save(output, format='JPEG', quality=90)
+                    output.seek(0)
+
+                    newPath = f'blur-{new_name}-{obj.vendor}-image.jpg'
+                    obj.image = InMemoryUploadedFile(output, 'ImageField', f"blur-{new_name}-{obj.vendor}.jpg", 'image/jpeg', sys.getsizeof(output), None)          
+                    os.remove(f"pil-{obj.name}-{obj.vendor}.jpg")
+
+
+
+                # rename the pdf file
+                obj.Guitar_Pro_Unlocked.name = f'guitar-pro-{new_name}-{obj.vendor}-unlocked.gp'
+                if obj.Guitar_Pro_Locked:
+                    obj.Guitar_Pro_Locked.name = f'guitar-pro-{new_name}-{obj.vendor}-locked.gp'
+
+                obj.PDF = request.FILES['PDF']
+                obj.PDF.name = f'{new_name}-{obj.vendor}.pdf'
+
+                obj.save()
                 messages.success(request, 'Successfully updated product!')
                 return redirect(reverse('product_detail', args=[product.id]))
             else:
