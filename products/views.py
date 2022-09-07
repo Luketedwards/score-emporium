f56ccefa75ee29c57f7ff1f534a475780761011f
Luketedwards <Luketedwards96@hotmail.com>
2022-09-07 09:17:07 +0000

    attempted different s3 upload method

--- products/views.py
+++ products/views.py
@@ -266,7 +266,7 @@ def add_product(request):
                 editImage.text((550,2100), text,(84, 83, 82), font=font)
                 editImage2 = ImageDraw.Draw(image)
                 editImage2.text((850,2600), text2,(84, 83, 82), font=font2)
-                image = fs.save(f"{new_name}-{obj.vendor}.jpg", image)
+                
                 newPath = f'blur-{new_name}-{obj.vendor}-image.jpg'
                 upload_file_s3(image, newPath)
                 
