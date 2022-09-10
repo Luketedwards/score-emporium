9a272da8fa3db92074850afab6c70c8f6c800b8a
Luketedwards <Luketedwards96@hotmail.com>
2022-09-10 13:01:14 +0000

    styled index.html

--- static/js/app.js
+++ static/js/app.js
@@ -13,6 +13,13 @@ $(".purchased-img").mouseover(function(){
   
 });
 
+// when hovering #hero-text apply hover to .overlay-hero
+$("#hero-text").mouseover(function(){
+  $(".overlay-hero").css("opacity", "0.7");
+}).mouseleave(function(){
+  $(".overlay-hero").css("opacity", "0.9");
+});
+
 
 
     document.getElementById('data-target0').className = 'active';
