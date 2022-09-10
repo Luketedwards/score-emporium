49d980f020f4cda57bd4648b87f733dc065f7c1a
Luketedwards <Luketedwards96@hotmail.com>
2022-09-10 19:18:33 +0000

    created new profile header for storefront

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,23 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
   // when hovering purchased-img class, change overflow to visible on purchased-details
-$(".purchased-img").mouseover(function(){
-  $("#purchased-details").css("overflow-y", "visible");
-  $('.purchased-img').css("z-index", "1");
-  
-}).mouseleave(function(){
-  setTimeout(function(){
-    $("#purchased-details").css("overflow-y", "scroll");
-    
- }, 200);
-  
-});
+  $(".purchased-img").mouseover(function () {
+    $("#purchased-details").css("overflow-y", "visible");
+    $('.purchased-img').css("z-index", "1");
 
-// when hovering #hero-text apply hover to .overlay-hero
-$("#hero-text").mouseover(function(){
-  $(".overlay-hero").css("opacity", "0.7");
-}).mouseleave(function(){
-  $(".overlay-hero").css("opacity", "0.9");
-});
+  }).mouseleave(function () {
+    setTimeout(function () {
+      $("#purchased-details").css("overflow-y", "scroll");
 
+    }, 200);
 
-// on screen resize if #rotate display is not none reload page and scroll to top
-$(window).resize(function(){
-  if ($("#rotate").css("display") != "none") {
-    location.reload();
-    $(document).ready(function(){
-      $(window).scrollTop(0);
-      $("body").css("overflow", "hidden");
-    });
-    
-  }
-  else {
-    $("body").css("overflow-y", "scroll");
-  }
-});
+  });
+
+  // when hovering #hero-text apply hover to .overlay-hero
+  $("#hero-text").mouseover(function () {
+    $(".overlay-hero").css("opacity", "0.7");
+  }).mouseleave(function () {
+    $(".overlay-hero").css("opacity", "0.9");
+  });
 
 
 
@@ -46,100 +29,112 @@ $(window).resize(function(){
 
 
 
-    document.getElementById('data-target0').className = 'active';
+
+
+
+  document.getElementById('data-target0').className = 'active';
   document.getElementById('carousel-item0').className = 'carousel-item active';
   $("#carousel-item0").addClass("active");
-  $('#myModal').modal('show');  
+  $('#myModal').modal('show');
 
-  
 
-  var observer = new IntersectionObserver(function(entries) {
+
+  var observer = new IntersectionObserver(function (entries) {
     // isIntersecting is true when element and viewport are overlapping
     // isIntersecting is false when element and viewport don't overlap
-    if(entries[0].isIntersecting === true)
+    if (entries[0].isIntersecting === true)
       $('#5bar').removeClass('empty')
-      $('#5bar').addClass('full5')
-      $('#4bar').removeClass('empty')
-      $('#4bar').addClass('full4')
-      $('#3bar').removeClass('empty')
-      $('#3bar').addClass('full3')
-      $('#2bar').removeClass('empty')
-      $('#2bar').addClass('full2')
-      $('#1bar').removeClass('empty')
-      $('#1bar').addClass('full1')
-  }, { threshold: [0] });
-  
+    $('#5bar').addClass('full5')
+    $('#4bar').removeClass('empty')
+    $('#4bar').addClass('full4')
+    $('#3bar').removeClass('empty')
+    $('#3bar').addClass('full3')
+    $('#2bar').removeClass('empty')
+    $('#2bar').addClass('full2')
+    $('#1bar').removeClass('empty')
+    $('#1bar').addClass('full1')
+  }, {
+    threshold: [0]
+  });
+
   observer.observe(document.querySelector(".graphs"));
 
   /* code for converting star rating to input value of review*/
 
   document.getElementById('rating-5-click').addEventListener('click', rating5);
   document.getElementById('rating-4-click').addEventListener('click', rating4);
-   document.getElementById('rating-3-click').addEventListener('click', rating3);
-   document.getElementById('rating-2-click').addEventListener('click', rating2);
-   document.getElementById('rating-1-click').addEventListener('click', rating1);
-   document.getElementById('reviewSubmit').addEventListener('click', chooseRating);
+  document.getElementById('rating-3-click').addEventListener('click', rating3);
+  document.getElementById('rating-2-click').addEventListener('click', rating2);
+  document.getElementById('rating-1-click').addEventListener('click', rating1);
+  document.getElementById('reviewSubmit').addEventListener('click', chooseRating);
   var ratingNumber = document.getElementById('hidden-rating');
-  function rating5(){
-  ratingNumber.value='5';
-  console.log('it worked');
+
+  function rating5() {
+    ratingNumber.value = '5';
+    console.log('it worked');
   }
-  function rating4(){
-  ratingNumber.value='4';
-  console.log('it worked');
-  
+
+  function rating4() {
+    ratingNumber.value = '4';
+    console.log('it worked');
+
   }
-  function rating3(){
-  ratingNumber.value='3';
-  console.log(ratingNumber.value);
-  
-  
+
+  function rating3() {
+    ratingNumber.value = '3';
+    console.log(ratingNumber.value);
+
+
   }
-  function rating2(){
-  ratingNumber.value='2';
-  console.log('it worked');
-  
+
+  function rating2() {
+    ratingNumber.value = '2';
+    console.log('it worked');
+
   }
-  function rating1(){
-  ratingNumber.value='1';
-  console.log('it worked js');
-  
+
+  function rating1() {
+    ratingNumber.value = '1';
+    console.log('it worked js');
+
   }
-  function chooseRating(){
-    if (ratingNumber.value == ''){
+
+  function chooseRating() {
+    if (ratingNumber.value == '') {
       alert("Please choose a star rating")
     }
   }
 });
 $('.carousel').carousel({
-    interval: 100000,
-    pause: "false"
-  });
+  interval: 100000,
+  pause: "false"
+});
 
-  $(".product").hover(function(){
-    $(this).toggleClass("is-active");
-  });
-  
+$(".product").hover(function () {
+  $(this).toggleClass("is-active");
+});
 
-  
-  const example5 = document.getElementById('commentBox');
-  const example5sb = document.querySelector('#commentBox .scrollbox');
-  let example5IsScrolling = false;
-  
-  function setFade(event) {
-    if (!example5IsScrolling) {
-      window.requestAnimationFrame(function() {
-        if (event.target.scrollTop < 160) {
-          example5.classList.add('show-icon');
-        }
-        else {
-          example5.classList.remove('show-icon');
-        }
-        example5IsScrolling = false;
-      });
-      example5IsScrolling = true;
-    }
+
+
+const example5 = document.getElementById('commentBox');
+const example5sb = document.querySelector('#commentBox .scrollbox');
+let example5IsScrolling = false;
+
+function setFade(event) {
+  if (!example5IsScrolling) {
+    window.requestAnimationFrame(function () {
+      if (event.target.scrollTop < 160) {
+        example5.classList.add('show-icon');
+      } else {
+        example5.classList.remove('show-icon');
+      }
+      example5IsScrolling = false;
+    });
+    example5IsScrolling = true;
   }
-  
-  example5sb.addEventListener('scroll', setFade);
+}
+
+example5sb.addEventListener('scroll', setFade);
+
+
 
---
+++ static/js/guitar_pro.js
@@ -0,0 +1,16 @@
+  //on load
+  $(document).ready(function () {
+      // on screen resize if #rotate display is not none reload page and scroll to top
+      $(window).resize(function () {
+          if ($("#rotate").css("display") != "none") {
+              location.reload();
+              $(document).ready(function () {
+                  $(window).scrollTop(0);
+                  $("body").css("overflow", "hidden");
+              });
+
+          } else {
+              $("body").css("overflow-y", "scroll");
+          }
+      });
+  });
